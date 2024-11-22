766a8c819d4c28fe6a97640129878dbf30a288d1
Valtemir <valmartinsfilho@gmail.com>
2024-11-22 18:59:31 -0300

    Trocando imagens e reultados

--- script.js
+++ script.js
@@ -30,7 +30,23 @@ function convertValues(){
     style: "currency",
     currency: "BRL"
   }).format(inputCurrencyValue)
-
 }
 
+function changeCurrency() {
+  const currencyName = document.getElementById("currency-name")
+  const currencyImage = document.querySelector(".currency-img")
+
+  if (currencySelect.value == "dolar"){
+    currencyName.innerHTML = "Dólar americano"
+    currencyImage.src = "./assets/dolar.png"
+  }
+
+  if (currencySelect.value == "euro"){
+    currencyName.innerHTML = "Euro"
+    currencyImage.src = "./assets/euro.png"
+  }
+  convertValues()
+}
+
+currencySelect.addEventListener("change",changeCurrency)
 convertButton.addEventListener("click", convertValues)
