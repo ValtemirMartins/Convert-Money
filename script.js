90a5f4b626ff9e365ff0d7bfae5f2a6484cc36e0
Valtemir <valmartinsfilho@gmail.com>
2024-11-22 18:31:14 -0300

    Adicionando converção para euro

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues(){
   const inputCurrencyValue = document.querySelector(".input-currency").value
@@ -6,20 +7,29 @@ function convertValues(){
   const currencyValueToConvert = document.querySelector(".currency-value-to-convert")  //valor em real
   const currencyValueConverted = document.querySelector(".currency-value") //outras moedas 
 
-
+  console.log(currencySelect.value)
   const dolarToday = 5.81
+  const euroToday = 6.04
 
-  const convertedValue = inputCurrencyValue / dolarToday
+
+  if (currencySelect.value == "dolar") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
+      style: "currency",
+      currency:"USD"
+    }).format(inputCurrencyValue / dolarToday)
+  }
+
+  if (currencySelect.value == "euro") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
+      style: "currency",
+      currency:"EUR"
+    }).format(inputCurrencyValue / euroToday)
+  }
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
     style: "currency",
     currency: "BRL"
   }).format(inputCurrencyValue)
-
-  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
-    style: "currency",
-    currency:"USD"
-  }).format(convertedValue)
 
 }
 
