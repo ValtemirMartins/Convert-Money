3476f07e90f923ed7e72fa3ab8af97f74361975e
Valtemir <valmartinsfilho@gmail.com>
2024-11-22 18:09:50 -0300

    formatando os numeros e trazendo informações

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues(){
   const inputCurrencyValue = document.querySelector(".input-currency").value
-   
+  
+  const currencyValueToConvert = document.querySelector(".currency-value-to-convert")  //valor em real
+  const currencyValueConverted = document.querySelector(".currency-value") //outras moedas 
+
+
   const dolarToday = 5.81
 
   const convertedValue = inputCurrencyValue / dolarToday
 
-  console.log(convertedValue)
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+    style: "currency",
+    currency: "BRL"
+  }).format(inputCurrencyValue)
+
+  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
+    style: "currency",
+    currency:"USD"
+  }).format(convertedValue)
+
 }
 
 convertButton.addEventListener("click", convertValues)
